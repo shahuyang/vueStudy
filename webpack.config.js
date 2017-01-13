c81395e2ade6ee762d009fa53010558e332687f8
shahuyang <shahuyang_c@163.com>
2017-01-14 03:47:08 +0800

    mac first commit

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var webpack = require('webpack')
+// 自定义公共模块提取
+// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'common', /* filename= */'common.js'); // 分析以下模块的共用代码, 单独打一个包到common.js
+var ExtractTextPlugin = require("extract-text-webpack-plugin"); // 单独打包CSS
+var HtmlWebpackPlugin = require('html-webpack-plugin'); // Html文件处理
+var webpack = require('webpack')
+// 自定义公共模块提取
+// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'common', /* filename= */'common.js'); // 分析以下模块的共用代码, 单独打一个包到common.js
+var ExtractTextPlugin = require("extract-text-webpack-plugin"); // 单独打包CSS
+var HtmlWebpackPlugin = require('html-webpack-plugin'); // Html文件处理
+
+
+module.exports = {
+    entry: './dev/src/js/entry.js',
+    output: {
+        path: __dirname,
+        filename: './dev/src/js/bundle.js'
+    },
+    plugins: [
+        new ExtractTextPlugin('./dev/src/css/[name].css', {allChunks: true}),
+        // commonsPlugin,
+        new HtmlWebpackPlugin({filename: './dev/templates/assessment.html', template: './dev/templates/highAssessment.html', hash: true}),
+    ],
+    module: {
+        loaders: [
+            { test: require.resolve("./dev/src/libs/zepto.v1.1.6.js"),  loader: "exports?$"},
+            { test: require.resolve("./dev/src/js/modules/JSBridge.js"),  loader: "exports?JSBridge"},
+            // css 打包进html 中
+            // { test: /\.css$/, loader: 'style-loader!css-loader' },
+            // css 文件单独打包
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+            //.js 文件使用 jsx-loader 来编译处理
+            { test: /\.js$/, loader: 'jsx-loader?harmony' },
+            //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
+            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+        ]
+    }
+}
+
+
+
+
+
+
+module.exports = {
+    entry: './dev/src/v0.0.3/js/entry.js',
+    output: {
+        path: __dirname,
+        filename: './dev/src/v0.0.3/js/bundle.js'
+    },
+    plugins: [
+        new ExtractTextPlugin('./dev/src/v0.0.3/css/[name].css', {allChunks: true}),
+        // commonsPlugin,
+        new HtmlWebpackPlugin({filename: './dev/templates/assessment.html', template: './dev/templates/highAssessment.html', hash: true}),
+    ],
+    module: {
+        loaders: [
+            { test: require.resolve("./dev/src/libs/zepto.v1.1.6.js"),  loader: "exports?$"},
+            { test: require.resolve("./dev/src/v0.0.3/js/modules/JSBridge.js"),  loader: "exports?JSBridge"},
+            // css 打包进html 中
+            // { test: /\.css$/, loader: 'style-loader!css-loader' },
+            // css 文件单独打包
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+            //.js 文件使用 jsx-loader 来编译处理
+            { test: /\.js$/, loader: 'jsx-loader?harmony' },
+            //.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
+            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+        ]
+    }
+}
+
+
